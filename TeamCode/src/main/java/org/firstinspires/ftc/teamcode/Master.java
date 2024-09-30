5535c690231edd0ff603e22d45934f2e066e4a69
ftcaarya <rautaarya23@gmail.com>
2024-09-29 23:30:14 -0400

    Re-wrote the whole files, made a master class which stores everything should be good now. Added servo constants and objects. Need to map to gamepad buttons.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Master.java
@@ -0,0 +1,151 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+public class Master extends LinearOpMode {
+
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    DcMotor rearLeft;
+    DcMotor rearRight;
+
+    IMU imu;
+
+    // port 1 control hub
+    public Servo leftClaw;
+    //port2 control hub
+    public Servo rightClaw;
+    //port0 control hub
+    public Servo wrist;
+    // port 3 control hub
+    public Servo lebronJames;
+    //port 0 expansion hub
+    public Servo bethBurry;
+    //port 1 expansion hub
+    public Servo outWrist;
+    // port 2 expansion hub
+    public Servo outRightClaw;
+    // port 3 expansion hub
+    public Servo outLeftClaw;
+
+    public static class ServoParams {
+        public static final double LEFT_CLAW_CLOSE = 0.4;
+        public static final double LEFT_CLAW_OPEN = 0;
+
+        public static final double RIGHT_CLAW_CLOSE = 0.6;
+        public static final double RIGHT_CLAW_OPEN = 0;
+
+        public static final double WRIST_UP = 1;
+        public static final double WRIST_DOWN = 0.5;
+
+        public static final double PIVOT_UP = 1;
+        public static final double PIVOT_DOWN = 0;
+
+        public static final double OUT_LEFT_CLAW_OPEN = 0;
+        public static final double OUT_LEFT_CLAW_CLOSE = 1;
+
+        public static final double OUT_RIGHT_CLAW_OPEN = 0;
+        public static final double OUT_RIGHT_CLAW_CLOSE = 1;
+
+        public static final double OUT_PIVOT_UP = 0;
+        public static final double OUT_PIVOT_DOWN = 1;
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+
+    public void motorInit() {
+        frontLeft = hardwareMap.get(DcMotor.class, "left front");
+        frontRight = hardwareMap.get(DcMotor.class, "right front");
+        rearLeft = hardwareMap.get(DcMotor.class, "left rear");
+        rearRight = hardwareMap.get(DcMotor.class, "right rear");
+
+        leftClaw = hardwareMap.get(Servo.class, "left claw servo");
+        rightClaw = hardwareMap.get(Servo.class, "right claw servo");
+        wrist = hardwareMap.get(Servo.class, "wrist servo");
+        lebronJames = hardwareMap.get(Servo.class, "pivot servo");
+
+        bethBurry = hardwareMap.get(Servo.class, "outtake servo");
+        outLeftClaw = hardwareMap.get(Servo.class, "out left claw servo");
+        outRightClaw = hardwareMap.get(Servo.class, "out right claw servo");
+        outWrist = hardwareMap.get(Servo.class, "out wrist servo");
+
+        // Change to match drivetrain
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        rearLeft.setDirection(DcMotor.Direction.REVERSE);
+        rearRight.setDirection(DcMotor.Direction.FORWARD);
+
+        imu = hardwareMap.get(IMU.class, "imu");
+
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+
+        // initialize the new parameter or go with the assumed - logo up / USB forward
+        imu.initialize(parameters);
+    }
+
+    public void centricMovement(Boolean fieldCentric) {
+
+        motorInit();
+
+        if (fieldCentric == Boolean.TRUE) {
+
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
+
+            // just to reset for driver control.
+            if (gamepad1.start) {
+                imu.resetYaw();
+            }
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            // Rotate the movement direction counter to the bot's rotation
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            // clips the power to not exceed the maximum value.
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+
+            frontLeft.setPower(frontLeftPower);
+            rearLeft.setPower(backLeftPower);
+            frontRight.setPower(frontRightPower);
+            rearRight.setPower(backRightPower);
+
+        } else if (fieldCentric == Boolean.FALSE) {
+
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
+
+            // Clipping the power to make sure it doesn't exceed the maximum value
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            frontLeft.setPower(frontLeftPower);
+            rearLeft.setPower(backLeftPower);
+            frontRight.setPower(frontRightPower);
+            rearRight.setPower(backRightPower);
+
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ServoTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ServoTesting.java
@@ -1,39 +1,41 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-import static org.firstinspires.ftc.teamcode.teleop.TeleOp.LEFT_CLAW_CLOSE;
-import static org.firstinspires.ftc.teamcode.teleop.TeleOp.LEFT_CLAW_OPEN;
-import static org.firstinspires.ftc.teamcode.teleop.TeleOp.RIGHT_CLAW_CLOSE;
-import static org.firstinspires.ftc.teamcode.teleop.TeleOp.RIGHT_CLAW_OPEN;
+import static org.firstinspires.ftc.teamcode.Master.ServoParams.LEFT_CLAW_CLOSE;
+import static org.firstinspires.ftc.teamcode.Master.ServoParams.LEFT_CLAW_OPEN;
+import static org.firstinspires.ftc.teamcode.Master.ServoParams.RIGHT_CLAW_CLOSE;
+import static org.firstinspires.ftc.teamcode.Master.ServoParams.RIGHT_CLAW_OPEN;
+
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.Master;
+
 public class ServoTesting extends LinearOpMode {
 
-    TeleOp servoConstants = new TeleOp();
+    Master robot = new Master();
 
     @Override
     public void runOpMode() throws InterruptedException {
-        servoConstants.leftClaw = hardwareMap.get(Servo.class, "left claw servo");
-        servoConstants.rightClaw = hardwareMap.get(Servo.class, "right claw servo");
-        servoConstants.wrist = hardwareMap.get(Servo.class, "wrist servo");
-        servoConstants.lebronJames = hardwareMap.get(Servo.class, "pivot servo");
 
-        servoConstants.bethBurry = hardwareMap.get(Servo.class, "outtake servo");
-        servoConstants.outLeftClaw = hardwareMap.get(Servo.class, "out left claw servo");
-        servoConstants.outRightClaw = hardwareMap.get(Servo.class, "out right claw servo");
-        servoConstants.outWrist = hardwareMap.get(Servo.class, "out wrist servo");
+        robot.motorInit();
 
+        waitForStart();
 
+        while (opModeIsActive()) {
+            if (gamepad1.left_bumper) {
+                robot.leftClaw.setPosition(LEFT_CLAW_OPEN);
+                robot.rightClaw.setPosition(RIGHT_CLAW_OPEN);
+            } else if (gamepad1.right_bumper) {
+                robot.leftClaw.setPosition(LEFT_CLAW_CLOSE);
+                robot.rightClaw.setPosition(RIGHT_CLAW_CLOSE);
+            }
 
-        if (gamepad1.left_bumper) {
-            servoConstants.leftClaw.setPosition(LEFT_CLAW_OPEN);
-            servoConstants.rightClaw.setPosition(RIGHT_CLAW_OPEN);
-        } else if (gamepad1.right_bumper) {
-            servoConstants.leftClaw.setPosition(LEFT_CLAW_CLOSE);
-            servoConstants.rightClaw.setPosition(RIGHT_CLAW_CLOSE);
         }
 
 
+
     }
+
+
 }
