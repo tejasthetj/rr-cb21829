11780787b9d6c97ca5fe1f62f5baeba0be953590
ftcaarya <rautaarya23@gmail.com>
2024-10-20 18:45:53 -0400

    simple auto rr traj testing code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoTrajTesting/AutoTrajTesting.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.autoTrajTesting;
+
+import androidx.annotation.NonNull;
+
+// RR-specific imports
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+
+// Non-RR imports
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+@Config
+@Autonomous(name = "BLUE_TEST_AUTO_PIXEL", group = "Autonomous")
+public class AutoTrajTesting extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Pose2d initialPose = new Pose2d(11.8, 61.7, Math.toRadians(90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+
+        TrajectoryActionBuilder myTraj = drive.actionBuilder(initialPose)
+                .lineToY(37)
+                .setTangent(Math.toRadians(0))
+                .lineToX(18)
+                .waitSeconds(3)
+                .setTangent(Math.toRadians(0))
+                .lineToXSplineHeading(46, Math.toRadians(180))
+                .waitSeconds(3);
+
+        waitForStart();
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        myTraj.build()
+                )
+        );
+
+    }
+}
