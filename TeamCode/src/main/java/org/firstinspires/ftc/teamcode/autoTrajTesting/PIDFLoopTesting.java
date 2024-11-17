1256ed0b087b7bdd698c8915c49aa73277c142e0
ftcaarya <rautaarya23@gmail.com>
2024-11-17 18:46:53 -0500

    changed version and added elevator PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoTrajTesting/PIDFLoopTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoTrajTesting/PIDFLoopTesting.java
@@ -5,71 +5,108 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.controller.PIDController;
-import com.arcrobotics.ftclib.controller.PIDFController;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-import org.opencv.core.Mat;
 
 
 @Config
 @Autonomous(name = "testing pid loop", group = "exercise")
 public class PIDFLoopTesting extends OpMode {
-    private PIDFController controllerLeft;
-    private PIDFController controllerRight;
+        private PIDController rightVertController;
+        private PIDController leftVertController;
+        private PIDController rightHorController;
+        private PIDController leftHorController;
+        public static double pv = 0.007, iv = 0, dv = 0.0002;
+        public static double ph = 0, ih = 0, dh = 0;
+        public static double fv = 0.05, fh = 0;
 
-    public static double p = 0, i = 0, d = 0, f = 0;
-    public static double p1 = 0, i1 = 0, d1 = 0, f1 = 0;
-
-    public static int target = 0;
-
-    private final double ticks_in_degree = 537.6 / 180.0;
-
-    public DcMotorEx elevatorLeft, elevatorRight;
-
-
-    @Override
-    public void init() {
-        controllerLeft = new PIDFController(p, i, d, f);
-        controllerRight = new PIDFController(p1, i1, d1, f1);
-        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        public static int vertTarget = 0;
+        public static int horTarget = 0;
 
 
-        elevatorLeft = hardwareMap.get(DcMotorEx.class, "vertical 2");
-        elevatorRight = hardwareMap.get(DcMotorEx.class, "vertical 1");
-        elevatorLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        elevatorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        elevatorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        private final double ticks_in_degrees = 576.7/180;
+        private DcMotor elevatorRight, elevatorLeft, horizontalRight, horizontalLeft;
 
 
-    }
 
-    @Override
-    public void loop() {
-        controllerLeft.setPIDF(p, i, d, f);
-        controllerRight.setPIDF(p, i, d, f);
-        int elevatorRightPos = elevatorRight.getCurrentPosition();
-        int elevatorLeftPos = elevatorLeft.getCurrentPosition();
+        @Override
+        public void init() {
+            rightVertController = new PIDController(pv,iv,dv);
+            leftVertController = new PIDController(pv, iv, dv);
+            rightHorController = new PIDController(ph,ih,dh);
+            leftHorController = new PIDController(ph,ih,dh);
+            telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
-        double pid = controllerLeft.calculate(elevatorLeftPos, target);
-        double pid2 = controllerRight.calculate(elevatorRightPos, target);
 
-        double ff = Math.cos(Math.toRadians(target)) * f;
-        double ff2 = Math.cos(Math.toRadians(target)) * f1;
+            elevatorRight = hardwareMap.get(DcMotor.class, "vertical 1");
+            elevatorLeft = hardwareMap.get(DcMotor.class, "vertical 2");
+            horizontalRight = hardwareMap.get(DcMotor.class, "horizontal 1");
+            horizontalLeft = hardwareMap.get(DcMotor.class, "horizontal 2");
+//            elevatorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//            elevatorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//            horizontalRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//            horizontalLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        double powerLeft = pid + ff;
-        double powerRight = pid2 + ff2;
 
-        elevatorRight.setPower(powerRight);
-        elevatorLeft.setPower(powerLeft);
 
-        telemetry.addData("left pos:", elevatorLeftPos);
-        telemetry.addData("right pos", elevatorRightPos);
-        telemetry.addData("target:", target);
-        telemetry.update();
+            horizontalRight.setDirection(DcMotorSimple.Direction.REVERSE);
+            elevatorLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
-    }
+
+
+        }
+
+        @Override
+        public void loop() {
+
+            rightVertController.setPID(pv,iv,dv);
+            leftVertController.setPID(pv,iv,dv);
+            rightHorController.setPID(ph,ih,dh);
+            leftHorController.setPID(ph,ih,dh);
+
+            int rightVertPos = elevatorRight.getCurrentPosition();
+            int leftVertPos = elevatorLeft.getCurrentPosition();
+            int rightHorPos = horizontalLeft.getCurrentPosition();
+            int leftHorPos = horizontalRight.getCurrentPosition();
+
+            double rightVertPid = rightVertController.calculate(rightVertPos,vertTarget);
+            double leftVertPid = leftVertController.calculate(leftVertPos,vertTarget);
+            double rightHorPid = rightHorController.calculate(rightHorPos, horTarget);
+            double leftHorPid = leftHorController.calculate(leftHorPos, horTarget);
+
+            double vertff = Math.cos(Math.toRadians(vertTarget / ticks_in_degrees)) * fv;
+            double horff = Math.cos(Math.toRadians(horTarget / ticks_in_degrees)) * fh;
+
+            double rightVertPower = rightVertPid + vertff;
+            double leftVertPower = leftVertPid + vertff;
+            double rightHorPower = rightHorPid + horff;
+            double leftHorPower = leftHorPid + horff;
+
+            elevatorRight.setPower(rightVertPower);
+            elevatorLeft.setPower(leftVertPower);
+            horizontalRight.setPower(rightHorPower);
+            horizontalLeft.setPower(leftHorPower);
+
+            telemetry.addData("Right vertical pos", rightVertPos);
+            telemetry.addData("Left vertical pos", leftVertPos);
+            telemetry.addData("Right horizontal pos", rightHorPos);
+            telemetry.addData("Left horizontal pos", leftHorPos);
+            telemetry.addData("Vetical Target", vertTarget);
+            telemetry.addData("Horizontal Target", horTarget);
+            telemetry.update();
+
+
+
+
+
+
+
+
+
+
+        }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/actionsTesting/EncoderCount.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/actionsTesting/EncoderCount.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.teleop.actionsTesting;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -8,12 +10,12 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp(name = "Encoder Count Tester", group = "exercises")
 public class EncoderCount extends LinearOpMode {
 
-    DcMotor elevatorRight, elevatorLeft;
+    private DcMotor elevatorRight, elevatorLeft, horizontalRight, horizontalLeft;
 
 
     @Override
     public void runOpMode() throws InterruptedException {
-
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         elevatorLeft = hardwareMap.get(DcMotor.class, "vertical 2");
         elevatorRight = hardwareMap.get(DcMotor.class, "vertical 1");
         elevatorLeft.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -21,6 +23,15 @@ public class EncoderCount extends LinearOpMode {
         elevatorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         elevatorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         elevatorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        horizontalRight = hardwareMap.get(DcMotor.class, "horizontal 1");
+        horizontalLeft = hardwareMap.get(DcMotor.class, "horizontal 2");
+        horizontalRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        horizontalLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        horizontalRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        horizontalLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
         telemetry.addData("Reset Values", "Done");
         telemetry.update();
 
@@ -28,8 +39,8 @@ public class EncoderCount extends LinearOpMode {
         waitForStart();
 
         while(opModeIsActive()) {
-            telemetry.addData("Left Elevator Encoder Count", elevatorLeft.getCurrentPosition());
-            telemetry.addData("Right Elevator Encoder Count", elevatorRight.getCurrentPosition());
+            telemetry.addData("Left Horizontal Encoder Count", horizontalLeft.getCurrentPosition());
+            telemetry.addData("Right Horizontal Encoder Count", horizontalRight.getCurrentPosition());
 
             telemetry.update();
         }
