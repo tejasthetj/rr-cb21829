5e9c4e6ec65456204fad7ba5eb1e6c6da80103d4
Aarya Raut <127800765+ftcaarya@users.noreply.github.com>
2024-11-26 07:29:31 -0500

    Update AllMechForRR.java
    trying to see if this is the problem because the docs mention that the motor is removed of power with this call.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoTrajTesting/AllMechForRR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoTrajTesting/AllMechForRR.java
@@ -74,12 +74,12 @@ public class AllMechForRR {
 
         elevatorLeft = hardwareMap.get(DcMotorEx.class, "vertical 2");
         elevatorRight = hardwareMap.get(DcMotorEx.class, "vertical 1");
-        elevatorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        elevatorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        elevatorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        elevatorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        elevatorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        elevatorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        // elevatorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // elevatorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // elevatorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        // elevatorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        // elevatorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        // elevatorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         elevatorLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
