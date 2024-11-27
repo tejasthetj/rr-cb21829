726ef51099465fb5b6461621f2cf0eaeda964071
ftcaarya <rautaarya23@gmail.com>
2024-11-27 18:15:22 -0500

    triyng horinztal pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoTrajTesting/PIDFLoopTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoTrajTesting/PIDFLoopTesting.java
@@ -4,6 +4,7 @@ package org.firstinspires.ftc.teamcode.autoTrajTesting;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.controller.PDController;
 import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -20,9 +21,10 @@ public class PIDFLoopTesting extends OpMode {
         private PIDController leftVertController;
         private PIDController rightHorController;
         private PIDController leftHorController;
+
         public static double pv = 0.007, iv = 0, dv = 0.0002;
-        public static double ph = 0.006, ih = 0, dh = 0.002;
-        public static double fv = 0.05, fh = 0.001;
+        public static double ph = 0.006, dh = 0.002;
+        public static double fv = 0.05;
 
         public static int vertTarget = 0;
         public static int horTarget = 0;
@@ -37,8 +39,8 @@ public class PIDFLoopTesting extends OpMode {
         public void init() {
             rightVertController = new PIDController(pv,iv,dv);
             leftVertController = new PIDController(pv, iv, dv);
-            rightHorController = new PIDController(ph,ih,dh);
-            leftHorController = new PIDController(ph,ih,dh);
+            rightHorController = new PIDController(ph,0,dh);
+            leftHorController = new PIDController(ph,0,dh);
             telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
 
@@ -67,8 +69,8 @@ public class PIDFLoopTesting extends OpMode {
 
             rightVertController.setPID(pv,iv,dv);
             leftVertController.setPID(pv,iv,dv);
-            rightHorController.setPID(ph,ih,dh);
-            leftHorController.setPID(ph,ih,dh);
+            rightHorController.setPID(ph,0,dh);
+            leftHorController.setPID(ph,0,dh);
 
             int rightVertPos = elevatorRight.getCurrentPosition();
             int leftVertPos = elevatorLeft.getCurrentPosition();
@@ -81,17 +83,14 @@ public class PIDFLoopTesting extends OpMode {
             double leftHorPid = leftHorController.calculate(leftHorPos, horTarget);
 
             double vertff = Math.cos(Math.toRadians(vertTarget / ticks_in_degrees)) * fv;
-            double horff = Math.cos(Math.toRadians(horTarget / ticks_in_degrees)) * fh;
 
             double rightVertPower = rightVertPid + vertff;
             double leftVertPower = leftVertPid + vertff;
-            double rightHorPower = rightHorPid + horff;
-            double leftHorPower = leftHorPid + horff;
 
             elevatorRight.setPower(rightVertPower);
             elevatorLeft.setPower(leftVertPower);
-            horizontalRight.setPower(rightHorPower);
-            horizontalLeft.setPower(leftHorPower);
+            horizontalRight.setPower(rightHorPid);
+            horizontalLeft.setPower(leftHorPid);
 //            telemetry.addData("Right vertical pos", rightVertPos);
 //            telemetry.addData("Left vertical pos", leftVertPos);
             telemetry.addData("Right horizontal pos", rightHorPos);
