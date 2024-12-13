920d337828568ab9575b5c09fed218a11059cd59
ftcaarya <rautaarya23@gmail.com>
2024-12-13 08:14:13 -0500

    new trajectory testing

--- meepmeeptesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ meepmeeptesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -31,18 +31,50 @@ public class MeepMeepTesting {
 //                .strafeToLinearHeading(new Vector2d(-55, -55), Math.toRadians(45))
 //                .build());
 
+        // testing
+//        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(10, -63, Math.toRadians(270)))
+//                .strafeToLinearHeading(new Vector2d(0,-36.5),Math.toRadians(270))
+//                // next thing
+//                .strafeTo(new Vector2d(20,-36.5))
+//                .splineToSplineHeading(new Pose2d(46,-13,Math.toRadians(270)),Math.toRadians(0))
+//                .strafeToLinearHeading(new Vector2d(46,-53),Math.toRadians(270))
+//                .waitSeconds(0.5)
+//                .strafeTo(new Vector2d(44,-13))
+//                .splineToLinearHeading(new Pose2d(55,-11.5,Math.toRadians(270)),Math.toRadians(0))
+//                .strafeToLinearHeading(new Vector2d(55,-53),Math.toRadians(270))
+//                .strafeToLinearHeading(new Vector2d(55,-33),Math.toRadians(0))
+//                // next thing
+//                .strafeToLinearHeading(new Vector2d(36,-56.5),Math.toRadians(0))
+//                //next thing
+//                .strafeToLinearHeading(new Vector2d(0,-36.5),Math.toRadians(270))
+//                // next thing
+//                .strafeToLinearHeading(new Vector2d(36,-56.5),Math.toRadians(0))
+//                // next thing
+//                .strafeToLinearHeading(new Vector2d(0,-36.5),Math.toRadians(270))
+//                // next thiing
+//                .strafeToLinearHeading(new Vector2d(36,-56.5),Math.toRadians(0))
+//                // wef
+//                .strafeToLinearHeading(new Vector2d(0,-36.5),Math.toRadians(270))
+//                // samplep get
+//                .strafeToLinearHeading(new Vector2d(36,-56.5),Math.toRadians(0))
+//                .build());
+
         // red right side
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(10, -63, Math.toRadians(270)))
                 .strafeTo(new Vector2d(0, -34))
                 .setReversed(false)
                 .splineToLinearHeading(new Pose2d(48, -38, Math.toRadians(90)), Math.PI / 4)
                 .strafeToLinearHeading(new Vector2d(48, -55), Math.toRadians(90))
-                .strafeToConstantHeading(new Vector2d(58, -38))
+                .setReversed(false)
+                .splineToLinearHeading(new Pose2d(40, -10, Math.toRadians(0)), Math.PI/2)
+                .strafeToLinearHeading(new Vector2d(58, -10), Math.toRadians(270))
                 .strafeToConstantHeading(new Vector2d(58, -55))
-                .setReversed(true)
-                .splineToSplineHeading(new Pose2d(50, -30, Math.toRadians(0)), Math.PI/2)
-                .splineToLinearHeading(new Pose2d(63, -10, Math.toRadians(-90)), Math.PI/6)
-                .strafeToConstantHeading(new Vector2d(62, -55))
+//                .strafeToConstantHeading(new Vector2d(58, -38))
+//                .strafeToConstantHeading(new Vector2d(58, -55))
+//                .setReversed(true)
+//                .splineToSplineHeading(new Pose2d(50, -30, Math.toRadians(0)), Math.PI/2)
+//                .splineToLinearHeading(new Pose2d(63, -10, Math.toRadians(-90)), Math.PI/6)
+//                .strafeToConstantHeading(new Vector2d(62, -55))
                 .strafeToLinearHeading(new Vector2d(25, -58), Math.toRadians(0))
                 .strafeToLinearHeading(new Vector2d(40, -58), Math.toRadians(0))
                 .strafeToLinearHeading(new Vector2d(0, -34), Math.toRadians(-90))
